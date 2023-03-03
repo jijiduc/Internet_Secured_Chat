477f29e97fd284c96b751baf8ec48a851bc18963
SebDuFond <sebborloz01@gmail.com>
2023-03-03 11:26:12 +0100

    ajout interface graphique

--- Main.py
+++ Main.py
@@ -1,10 +1,6 @@
 """""
 C'est le fichier main, qui appelle toutes les fonctions et fait tourner le programme
 """""
+from UI.FNETR import *
 
-from UI.button import *
 
-#app = QApplication(sys.argv)
-window = Ui_Dialog()
-window.setupUi(Dialog: {objectName, resize, setWindowTitle})
-#sys.exit(app.exec())
